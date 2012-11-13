a28415e01b1fa8f01f004676dd4b6b924148fe22
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-11-13 17:19:24 -0600

    more thorough tests and stronger integer detection

--- test/test.js
+++ test/test.js
@@ -3,14 +3,22 @@ var is = require('../lib/is')
 
 describe('numbers', function(){
 
-  it('is.integer', function(){
+  it('#integer', function(){
+    var fn = function(){}
+    assert.equal(false, is.integer(fn))
+    assert.equal(false, is.integer(new Date))
     assert.equal(false, is.integer(undefined))
     assert.equal(false, is.integer(NaN))
     assert.equal(false, is.integer(null))
+    assert.equal(false, is.integer(true))
+    assert.equal(false, is.integer(false))
     assert.equal(false, is.integer({}))
+    assert.equal(false, is.integer([]))
+    assert.equal(false, is.integer(""))
     assert.equal(false, is.integer("asdf"))
     assert.equal(false, is.integer(1.23))
     assert.equal(true,  is.integer(-1))
+    assert.equal(true,  is.integer(0))
     assert.equal(true,  is.integer(666))
   })
 
