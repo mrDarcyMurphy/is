c1ce7d73b86ecd81036d45db0083e91c3b51ff38
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-02-28 13:41:02 -0600

    string tests

--- test/test.js
+++ test/test.js
@@ -150,6 +150,27 @@ describe('numbers', function(){
     assert.equal(false, is.zero(666)) // :metal:
   })
 
+  it('#string', function(){
+    var fn = function(){}
+    assert.equal(false, is.string(fn))
+    assert.equal(false, is.string(new Date))
+    assert.equal(false, is.string(undefined))
+    assert.equal(false, is.string(NaN))
+    assert.equal(false, is.string(null))
+    assert.equal(false, is.string(true))
+    assert.equal(false, is.string(false))
+    assert.equal(false, is.string({}))
+    assert.equal(false, is.string([]))
+    assert.equal(true,  is.string(""))
+    assert.equal(true,  is.string(" 	"))
+    assert.equal(true, is.string("asdf"))
+    assert.equal(false, is.string(1.23))
+    assert.equal(false, is.string(-42))
+    assert.equal(false, is.string(-1))
+    assert.equal(false, is.string(0))
+    assert.equal(false, is.string(666)) // :metal:
+  })
+
   it('#emptyString', function(){
     var fn = function(){}
     assert.equal(false, is.emptyString(fn))
