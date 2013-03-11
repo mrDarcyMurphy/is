abc9ab8be033a340d638aa6fcfad252a43752461
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-03-10 21:52:37 -0500

    cleaned up primitives testing

--- test/primitives.js
+++ test/primitives.js
@@ -1,52 +1,31 @@
 var assert = require('assert')
 var is = require('../lib/is')
+var fn = function(){}
 
 describe('primitives', function(){
 
-  it('string', function(){
-    var fn = function(){}
-    assert.equal(false, is.string(fn))
-    assert.equal(false, is.string(new Date))
-    assert.equal(false, is.string(undefined))
-    assert.equal(false, is.string(NaN))
-    assert.equal(false, is.string(null))
-    assert.equal(false, is.string(true))
-    assert.equal(false, is.string(false))
-    assert.equal(false, is.string({}))
-    assert.equal(false, is.string([]))
-    assert.equal(true,  is.string(""))
-    assert.equal(true,  is.string("   "))
-    assert.equal(true,  is.string("asdf"))
-    assert.equal(false, is.string(1.23))
-    assert.equal(false, is.string(-42))
-    assert.equal(false, is.string(-1))
-    assert.equal(false, is.string(0))
-    assert.equal(false, is.string(666)) // :metal:
-  })
-
-  it('function', function(){
-    var fn = function(){}
-    assert.equal(true,  is.func(fn))
-    assert.equal(false, is.func(new Date))
-    assert.equal(false, is.func(undefined))
-    assert.equal(false, is.func(NaN))
-    assert.equal(false, is.func(null))
-    assert.equal(false, is.func(true))
-    assert.equal(false, is.func(false))
-    assert.equal(false, is.func({}))
-    assert.equal(false, is.func([]))
-    assert.equal(false, is.func(""))
-    assert.equal(false, is.func("   "))
-    assert.equal(false, is.func("asdf"))
-    assert.equal(false, is.func(1.23))
-    assert.equal(false, is.func(-42))
-    assert.equal(false, is.func(-1))
-    assert.equal(false, is.func(0))
-    assert.equal(false, is.func(666)) // :metal:
+  it('array', function(){
+    assert.equal(false, is.array(fn))
+    assert.equal(false, is.array(new Date))
+    assert.equal(false, is.array(undefined))
+    assert.equal(false, is.array(NaN))
+    assert.equal(false, is.array(null))
+    assert.equal(false, is.array(true))
+    assert.equal(false, is.array(false))
+    assert.equal(false, is.array({}))
+    assert.equal(false, is.array({foo:'bar'}))
+    assert.equal(true , is.array([]))
+    assert.equal(false, is.array(""))
+    assert.equal(false, is.array("   "))
+    assert.equal(false, is.array("asdf"))
+    assert.equal(false, is.array(1.23))
+    assert.equal(false, is.array(-42))
+    assert.equal(false, is.array(-1))
+    assert.equal(false, is.array(0))
+    assert.equal(false, is.array(666))
   })
 
   it('boolean', function(){
-    var fn = function(){}
     assert.equal(false, is.boolean(fn))
     assert.equal(false, is.boolean(new Date))
     assert.equal(false, is.boolean(undefined))
@@ -66,47 +45,65 @@ describe('primitives', function(){
     assert.equal(false, is.boolean(666)) // :metal:
   })
 
-  it('object', function(){
-    var fn = function(){}
-    assert.equal(true , is.object(fn))
-    assert.equal(true , is.object(new Date))
-    assert.equal(false, is.object(undefined))
-    assert.equal(false, is.object(NaN))
-    assert.equal(false, is.object(null))
-    assert.equal(false, is.object(true))
-    assert.equal(false, is.object(false))
-    assert.equal(true ,  is.object({}))
-    assert.equal(true , is.object([]))
-    assert.equal(false, is.object(""))
-    assert.equal(false, is.object("   "))
-    assert.equal(false, is.object("asdf"))
-    assert.equal(false, is.object(1.23))
-    assert.equal(false, is.object(-42))
-    assert.equal(false, is.object(-1))
-    assert.equal(false, is.object(0))
-    assert.equal(false, is.object(666)) // :metal:
+  it('function', function(){
+    assert.equal(true , is.function(fn))
+    assert.equal(false, is.function(new Date))
+    assert.equal(false, is.function(undefined))
+    assert.equal(false, is.function(NaN))
+    assert.equal(false, is.function(null))
+    assert.equal(false, is.function(true))
+    assert.equal(false, is.function(false))
+    assert.equal(false, is.function({}))
+    assert.equal(false, is.function([]))
+    assert.equal(false, is.function(""))
+    assert.equal(false, is.function("   "))
+    assert.equal(false, is.function("asdf"))
+    assert.equal(false, is.function(1.23))
+    assert.equal(false, is.function(-42))
+    assert.equal(false, is.function(-1))
+    assert.equal(false, is.function(0))
+    assert.equal(false, is.function(666)) // :metal:
   })
 
-  it('emptyObject', function(){
-    var fn = function(){}
-    assert.equal(true , is.emptyObject(fn))
-    assert.equal(true , is.emptyObject(new Date))
-    assert.equal(true , is.emptyObject(undefined))
-    assert.equal(true , is.emptyObject(NaN))
-    assert.equal(true , is.emptyObject(null))
-    assert.equal(true , is.emptyObject(true))
-    assert.equal(true , is.emptyObject(false))
-    assert.equal(true , is.emptyObject({}))
-    assert.equal(false, is.emptyObject({foo:'bar'}))
-    assert.equal(true , is.emptyObject([]))
-    assert.equal(true , is.emptyObject(""))
-    assert.equal(false, is.emptyObject("   "))
-    assert.equal(false, is.emptyObject("asdf"))
-    assert.equal(true , is.emptyObject(1.23))
-    assert.equal(true , is.emptyObject(-42))
-    assert.equal(true , is.emptyObject(-1))
-    assert.equal(true , is.emptyObject(0))
-    assert.equal(true , is.emptyObject(666)) // :metal:
+  it('null', function(){
+    assert.equal(false, is.null(fn))
+    assert.equal(false, is.null(new Date))
+    assert.equal(false, is.null(undefined))
+    assert.equal(false, is.null(NaN))
+    assert.equal(true , is.null(null))
+    assert.equal(false, is.null(true))
+    assert.equal(false, is.null(false))
+    assert.equal(false, is.null({}))
+    assert.equal(false, is.null([]))
+    assert.equal(false, is.null(""))
+    assert.equal(false, is.null("   "))
+    assert.equal(false, is.null("asdf"))
+    assert.equal(false, is.null(1.23))
+    assert.equal(false, is.null(-42))
+    assert.equal(false, is.null(-1))
+    assert.equal(false, is.null(0))
+    assert.equal(false, is.null(666)) // :metal:
   })
+
+  it('undefined', function(){
+    assert.equal(false, is.undefined(fn))
+    assert.equal(false, is.undefined(new Date))
+    assert.equal(true , is.undefined(undefined))
+    assert.equal(false, is.undefined(NaN))
+    assert.equal(false, is.undefined(null))
+    assert.equal(false, is.undefined(true))
+    assert.equal(false, is.undefined(false))
+    assert.equal(false, is.undefined({}))
+    assert.equal(false, is.undefined([]))
+    assert.equal(false, is.undefined(""))
+    assert.equal(false, is.undefined("   "))
+    assert.equal(false, is.undefined("asdf"))
+    assert.equal(false, is.undefined(1.23))
+    assert.equal(false, is.undefined(-42))
+    assert.equal(false, is.undefined(-1))
+    assert.equal(false, is.undefined(0))
+    assert.equal(false, is.undefined(666)) // :metal:
+  })
+
 
 })
