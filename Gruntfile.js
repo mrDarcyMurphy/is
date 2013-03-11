41c7d8b7b48d2f58a9399bc2e92e1f56dcd2cc1e
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-03-10 22:11:31 -0500

    pull in jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */'
@@ -15,8 +21,9 @@ module.exports = function(grunt) {
   })
 
   // Default task.
-  grunt.registerTask('default', ['uglify'])
+  grunt.registerTask('default', ['jshint', 'uglify'])
 
   // load the grunt task plugins
+  grunt.loadNpmTasks('grunt-contrib-jshint')
   grunt.loadNpmTasks('grunt-contrib-uglify')
 };
