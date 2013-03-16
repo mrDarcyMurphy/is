0a2931728d9d6af4a395fa6d47ff562506b0418c
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-03-16 18:24:11 -0500

    test non-existence of aliased helpers

--- test/antonyms.js
+++ test/antonyms.js
@@ -10,8 +10,16 @@ describe('isnt', function(){
     assert.equal(is.not.toNumber, undefined)
   })
 
+  it('has no toNum', function(){
+    assert.equal(is.not.toNum, undefined)
+  })
+
   it('has no toInteger', function(){
     assert.equal(is.not.toInteger, undefined)
+  })
+
+  it('has no toInt', function(){
+    assert.equal(is.not.toInt, undefined)
   })
 
 })
