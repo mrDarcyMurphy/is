7c09f14ce92eda2e034470771baf29dbeda9baca
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-09-23 22:53:39 -0500

    test more things

--- test/plainObject.js
+++ test/plainObject.js
@@ -2,9 +2,8 @@ var assert = require('assert')
 var is = require('../lib/is')
 var fn = function(){}
 
-var future = new Date('04/16/9999')
-var today = new Date()
-var past = new Date('04/16/1982')
+var exp = (666).toExponential()
+var hex = 0x1
 
 describe('plainObject', function(){
 
@@ -16,7 +15,10 @@ describe('plainObject', function(){
     assert.equal(false, is.plainObject(null), 'null is wrong')
     assert.equal(false, is.plainObject(true), 'true is wrong')
     assert.equal(false, is.plainObject(false), 'false is wrong')
-    assert.equal(true , is.plainObject({}), '{} is wrong')
+    assert.equal(false, is.plainObject(new Object()), "new Object() is wrong")
+    assert.equal(true , is.plainObject({}), "{} is wrong")
+    assert.equal(false, is.plainObject({foo:'bar'}), "{foo:'bar'} is wrong")
+    assert.equal(true , is.plainObject(new Array()), "new Array() is wrong")
     assert.equal(false, is.plainObject([]), '[] is wrong')
     assert.equal(false, is.plainObject(""), '"" is wrong')
     assert.equal(false, is.plainObject("  "), '"  " is wrong')
@@ -32,6 +34,9 @@ describe('plainObject', function(){
     assert.equal(false, is.plainObject(-1), '-1 is wrong')
     assert.equal(false, is.plainObject(0), '0 is wrong')
     assert.equal(false, is.plainObject(666), '666 is wrong')
+    assert.equal(false, is.plainObject(exp), 'exp is wrong')
+    assert.equal(false, is.plainObject(hex), 'hex is wrong')
+    assert.equal(false, is.plainObject(Infinity), 'Infinity is wrong')
   })
 
 })
