7990d737fade1a2cf9a4423c7d536d219bd229ac
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-04-20 11:00:21 -0500

    added regexp check

--- test/objects.js
+++ test/objects.js
@@ -1,71 +1,50 @@
 var asserts = require('./asserts.js')
 
+var objects = [
+  'newDate',
+  'nil',
+  'newBooleanTrue',
+  'newBooleanFalse',
+  'objectCreateNull',
+  'objectCreateObjectPrototype',
+  'newObject',
+  'emptyObject',
+  'plainObject',
+  'objectLengthOne',
+  'newArray',
+  'emptyArray',
+  'plainArray',
+  'args',
+  'rx'
+]
+
+var plainObjects = [
+  'newBooleanTrue',
+  'newBooleanFalse',
+  'objectCreateNull',
+  'objectCreateObjectPrototype',
+  'newObject',
+  'emptyObject',
+  'plainObject',
+  'objectLengthOne'
+]
+
 describe('objects', function(){
 
   it('is.object', function(){
-    asserts.is('object', [
-      'newDate',
-      'nil',
-      'newBooleanTrue',
-      'newBooleanFalse',
-      'objectCreateNull',
-      'objectCreateObjectPrototype',
-      'newObject',
-      'emptyObject',
-      'plainObject',
-      'objectLengthOne',
-      'newArray',
-      'emptyArray',
-      'plainArray',
-      'args',
-      'rx'
-    ])
+    asserts.is('object', objects)
   })
 
   it('is.not.object', function(){
-    asserts.not('object', [
-      'newDate',
-      'nil',
-      'newBooleanTrue',
-      'newBooleanFalse',
-      'objectCreateNull',
-      'objectCreateObjectPrototype',
-      'newObject',
-      'emptyObject',
-      'plainObject',
-      'objectLengthOne',
-      'newArray',
-      'emptyArray',
-      'plainArray',
-      'args',
-      'rx'
-    ])
+    asserts.not('object', objects)
   })
 
   it('is.plainObject', function(){
-    asserts.is('plainObject', [
-      'newBooleanTrue',
-      'newBooleanFalse',
-      'objectCreateNull',
-      'objectCreateObjectPrototype',
-      'newObject',
-      'emptyObject',
-      'plainObject',
-      'objectLengthOne'
-    ])
+    asserts.is('plainObject', plainObjects)
   })
 
   it('is.not.plainObject', function(){
-    asserts.not('plainObject', [
-      'newBooleanTrue',
-      'newBooleanFalse',
-      'objectCreateNull',
-      'objectCreateObjectPrototype',
-      'newObject',
-      'emptyObject',
-      'plainObject',
-      'objectLengthOne'
-    ])
+    asserts.not('plainObject', plainObjects)
   })
 
 })
--- test/subjects.js
+++ test/subjects.js
@@ -51,6 +51,7 @@ function subjects() {
     'infinityString'              : "Infinity",
     'positiveInfinityString'      : "+Infinity",
     'rx'                          : /^foo/,
+    'newRegexp'                   : new RegExp("^foo"),
     'rxString'                    : "^foo"
   }
 
