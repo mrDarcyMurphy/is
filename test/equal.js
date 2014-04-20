49c04017fd7c87ff3d909781058bfd82e313c472
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-04-20 11:21:48 -0500

    being a littls tupid with the tests, but was curious

--- test/equal.js
+++ test/equal.js
@@ -1,6 +1,7 @@
 var is = require('../lib/is')
 var assert = require('assert')
-var asserts = require('./asserts.js')
+var asserts = require('./asserts')
+var subjects = require('./subjects')
 
 var equals = ['nil', 'undef']
 var exactlies = ['undef']
@@ -9,11 +10,23 @@ describe('equality', function(){
 
   it('is.equal', function(){
     asserts.is('equal', equals)
+    assert(is.equal(subjects.negativeZero, subjects.zero))
+    assert(is.equal(subjects.negativeZero, subjects.positiveZero))
+  })
+
+  it('is.equal', function(){
+    asserts.not('equal', equals)
+    assert(is.not.equal(subjects.negativeExponent, subjects.positiveExponent))
   })
 
   it('is.exactly', function(){
     asserts.is('exactly', exactlies)
     assert(is.exactly(1, 1))
+    assert(is.exactly(subjects.negativeZero, subjects.positiveZero))
+  })
+
+  it('is.not.exactly', function(){
+    assert(is.not.exactly(subjects.negativeExponent, subjects.positiveExponent))
   })
 
 })
